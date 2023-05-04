2eabf4b7fc7fca7797e8071beaa9729760805d09
Nils Büchner <n.buechner@united-gameserver.de>
2023-05-04 18:53:57 +0200

    change latest.sh

--- latest.sh
+++ latest.sh
@@ -3,14 +3,13 @@ URL=$(curl -s https://api.github.com/repos/drwhut/tabletop-club/releases/latest 
 VERSION=$(echo $URL|cut -d'/' -f8|tr -d 'v')
 LOCAL_VERSION=$(cat snapcraft.yaml|grep "version: "|cut -d" " -f2)
 if [[ "$VERSION" != "$LOCAL_VERSION" ]]; then
-   #echo "local and remove versions differ"
-   #echo "old version: $LOCAL_VERSION"
-   #echo "new version: $VERSION"
-   #sed -i "/^\([[:space:]]*version: \).*/s//\1$VERSION/" snapcraft.yaml
-   #echo "git commit -a -m 'version update: $VERSION'"
-   #echo "git push"
-   echo "snapcraft upload --release=stable tabletop-club-unofficial_$VERSION_amd64.snap"
+   echo "local and remove versions differ"
+   echo "old version: $LOCAL_VERSION"
+   echo "new version: $VERSION"
+   sed -i "/^\([[:space:]]*version: \).*/s//\1$VERSION/" snapcraft.yaml
+   git commit -a -m 'version update: $VERSION'
+   git push
+   snapcraft upload --release=stable tabletop-club-unofficial_${VERSION}_amd64.snap
 else
    echo "no update available. current version is $VERSION"
 fi
-#SNAPCRAFT_BUILD_ENVIRONMENT=host snapcraft --target-arch amd64
