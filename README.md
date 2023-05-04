253bb5c7a55147d953a5e9ec4cdca4962cb25838
Nils Büchner <n.buechner@united-gameserver.de>
2023-05-04 18:17:49 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# tabletop-club-snap
---
+++ latest.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+URL=$(curl -s https://api.github.com/repos/drwhut/tabletop-club/releases/latest | jq '.assets[] | select(.name|endswith("_Linux_64.zip")) | .browser_download_url' | tr -d '"')
+VERSION=$(echo $URL|cut -d'/' -f8|tr -d 'v')
+LOCAL_VERSION=$(cat snapcraft.yaml|grep "version: "|cut -d" " -f2)
+if [[ "$VERSION" != "$LOCAL_VERSION" ]]; then
+   echo "local and remove versions differ"
+   echo "old version: $LOCAL_VERSION"
+   echo "new version: $VERSION"
+   sed -i "/^\([[:space:]]*version: \).*/s//\1$VERSION/" snapcraft.yaml
+   echo "git commit -a -m 'version update: $VERSION'"
+   echo "git push"
+else
+   echo "no update available. current version is $VERSION"
+fi
+#SNAPCRAFT_BUILD_ENVIRONMENT=host snapcraft --target-arch amd64
